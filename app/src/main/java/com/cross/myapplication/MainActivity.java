7011d4a9ab6d7642c7f44e31cbe8f7df43389e83
门闯 <1019866800@qq.com>
2023-09-24 12:39:52 +0800

    更新了一下传递设备ID的位置

--- app/src/main/java/com/cross/myapplication/MainActivity.java
+++ app/src/main/java/com/cross/myapplication/MainActivity.java
@@ -31,14 +31,13 @@ public class MainActivity extends AppCompatActivity {
         }
         Map<String, String> map = new ArrayMap<>();
         map.put("gameUserOs", "CROSS_AOS");
-        map.put("gameUserPk", DeviceIdUtils.getDeviceId(this));
 //            map.put("gameUserName", "啊啊啊");
 //        map.put("gameMediaPackage", packageInfo.packageName);
         map.put("gameMediaPackage", "com.cncsys.blez");
         map.put("gameUserGrade", "20.35");
         map.put("gameMediaIcon", "http://123.249.110.79:8080/jeecg-boot/sys/common/static/temp/qt_1689040494959_1689261898778.png");
         map.put("gameUserOsVersion", GCrossUtils.getSystemVersion());
-        LoginClass.login(new Gson().toJson(map), this);
+        LoginClass.login(map, this);
         findViewById(R.id.tvStart).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, GCrossActivity.class)));
 //            new GCrossHttpUtils(new Gson().toJson(map), "loginGameUser").loginGameUser();
 //        startActivity(new Intent(MainActivity.this, GCrossActivity.class));
--- gcross/src/main/java/com/cross/gcross/login/LoginClass.java
+++ gcross/src/main/java/com/cross/gcross/login/LoginClass.java
@@ -2,16 +2,21 @@ package com.cross.gcross.login;
 
 import android.content.Context;
 
+import com.cross.gcross.utils.DeviceIdUtils;
 import com.cross.gcross.utils.GCrossHttpConstant;
 import com.cross.gcross.utils.GCrossHttpUtils;
+import com.google.gson.Gson;
 import com.tencent.mmkv.MMKV;
+
+import java.util.Map;
 
 public class LoginClass {
 
-    public static void login(String json, Context context) {
+    public static void login(Map<String,String> map, Context context) {
         String dir = context.getFilesDir().getAbsolutePath() + "/GcrossMMKV";
         MMKV.initialize(context, dir);
         GCrossSharedPreferencesUtil.getInstance();
-        new GCrossHttpUtils(json, GCrossHttpConstant.LOGIN_GAME_USER).loginGameUser();
+        map.put("gameUserPk", DeviceIdUtils.getDeviceId(context));
+        new GCrossHttpUtils(new Gson().toJson(map), GCrossHttpConstant.LOGIN_GAME_USER).loginGameUser();
     }
 }
